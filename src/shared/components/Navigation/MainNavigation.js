ba0d8a0d79bedce00ed8eb9c3a7d3cabe9b7c081
ammarnasr <ammarnasraza@gmail.com>
2021-03-20 23:50:42 +0200

    Added Side Drawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import NavLinks from './NavLinks';
-import MainHeader from './MainHeader';
-import './MainNavigation.css';
+import SideDrawer from "./SideDrawer";
+import NavLinks from "./NavLinks";
+import MainHeader from "./MainHeader";
+import "./MainNavigation.css";
 
-const MainNavigation = props => {
-    return (
-        <MainHeader>
-            <button className="main-navigation__menu-btn">
-                <span/>
-                <span/>
-                <span/>
-            </button>
-            <h1 className="main-navigation__title">
-                <Link to="/">My Title</Link>
-            </h1>
-            <nav>
-                <NavLinks />
-            </nav>
-        </MainHeader>
-    );
-}
+const MainNavigation = (props) => {
+  return (
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">My Title</Link>
+        </h1>
+        <nav>
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
+  );
+};
 
 export default MainNavigation;
